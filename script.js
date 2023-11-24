1dc26b41583a0a6451e15991fb48ef116a77596e
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-24 13:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,42 @@ function mincost(arr)
 //write your code here
 // return the min cost
 
-	 if (arr.length === 1) {
-    return 0; // No cost to connect a single rope
+	  const n = arr.length;
+
+  // Initialize final cost variable
+  let finalCost = 0;
+
+  // Declaring a minHeap
+  const minHeap = [];
+
+  // Inserting all ropes to the minHeap
+  for (let i = 0; i < n; i++) {
+    minHeap.push(arr[i]);
   }
 
-  let minCost = Number.MAX_SAFE_INTEGER;
+  // Convert the array into a min-heap
+  minHeap.sort((a, b) => a - b);
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      const combinedLength = arr[i] + arr[j];
-      const newArr = [...arr.slice(0, i), combinedLength, ...arr.slice(j + 1)];
-      const cost = mincost(newArr);
-      minCost = Math.min(minCost, cost + combinedLength);
-    }
+  // Merge until there is only one rope left in minHeap
+  while (minHeap.length > 1) {
+    // Extract the smallest rope
+    const smallest = minHeap.shift();
+
+    // Extract the second smallest rope
+    const secondSmallest = minHeap.shift();
+
+    // Add cost of merging these two ropes to final answer
+    finalCost += smallest + secondSmallest;
+
+    // Insert the combined rope back in the minHeap
+    minHeap.push(smallest + secondSmallest);
+
+    // Re-sort the array to maintain the min-heap property
+    minHeap.sort((a, b) => a - b);
   }
 
-  return minCost;
+  // Return the finalCost
+  return finalCost;
   
 }
 
