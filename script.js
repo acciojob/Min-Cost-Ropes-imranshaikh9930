885327343f383994626c92d272f226140e2ab531
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-24 12:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	 if (arr.length === 1) {
+    return 0; // No cost to connect a single rope
+  }
+
+  let minCost = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const combinedLength = arr[i] + arr[j];
+      const newArr = [...arr.slice(0, i), combinedLength, ...arr.slice(j + 1)];
+      const cost = mincost(newArr);
+      minCost = Math.min(minCost, cost + combinedLength);
+    }
+  }
+
+  return minCost;
   
 }
 
